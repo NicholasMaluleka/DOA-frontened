74d86871b9d2c8f1c096daa05112abe7bb606588
Nicholas <lesetjamaluleka@gmail.com>
2024-09-12 09:30:07 +0200

    added table on director

--- app/src/app/config/declarations.ts
+++ app/src/app/config/declarations.ts
@@ -16,6 +16,8 @@ window['neutrinos'] = {
 };
 
 //CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-toolbar_2Component
+import { toolbar_2Component } from '../components/shared_components/toolbar_2.component';
 //CORE_REFERENCE_IMPORT-edit_profileComponent
 import { edit_profileComponent } from '../components/Client/edit_profile.component';
 //CORE_REFERENCE_IMPORT-password_pop_upComponent
@@ -122,6 +124,8 @@ export const appDeclarations = [
   PageNotFoundComponent,
   ArtImgSrcDirective,
   //CORE_REFERENCE_PUSH_TO_DEC_ARRAY
+  //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-toolbar_2Component
+  toolbar_2Component,
   //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-edit_profileComponent
   edit_profileComponent,
   //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-password_pop_upComponent
